896f13f91eadf2d5f858221b8ca6a876179dd17f
Tina Tavakkoli <tinatavakkoli81@gmail.com>
2023-07-05 09:14:55 +0430

    final code about Genetic Algorithm

--- main.py
+++ main.py
@@ -1,240 +1,54 @@
-import heapq
-import time
-from collections import deque
-from queue import PriorityQueue, Queue
-
-resources = []
-tasks = []
-Priority_queue = []
-waiting_queue = []
-queue = []
+import pandas as pd
+import random
+import pygad
+import numpy
 
 
-class Task:
-    def __init__(self, name, type, execution_time):
-        self.waiting_time = 0
-        if type == 'x':
-            self.type = 3
-        if type == 'y':
-            self.type = 2
-        if type == 'z':
-            self.type = 1
-        self.name = name
-        self.execution_time = execution_time
+def get_data():
+    train = pd.read_csv("train.csv")
+    test = pd.read_csv("test.csv")
 
-    def __str__(self):
-        return self.name
-    # def __lt__(self, other):
-    #     return self.execution_time < other.execution_time
+    return train, test
 
 
-def check_resources(task, resources):
-    if task.type == 3:
-        if resources[0] > 0 and resources[1] > 0:
-            return True
-    elif task.type == 2:
-        if resources[2] > 0 and resources[1] > 0:
-            return True
-    elif task.type == 1:
-        if resources[0] > 0 and resources[2] > 0:
-            return True
-    else:
-        return False
+def test(ga_instance, solution, solution_idx):
+    fitness = 0
+    for index, row in test_data.iterrows():
+        output = numpy.sum(solution[1:] * row[1:11]) + solution[0]
+        fitness += (output - row[11]) ** 2
+    return fitness
 
 
-def zero_resources(resources):
-    for i in resources:
-        if (i != 0):
-            return False
-    return True
+def fitness_func(ga_instance, solution, solution_idx):
+    fitness = 0
+    for index, row in train_data.iterrows():
+        output = numpy.sum(solution[1:] * row[1:11]) + solution[0]
+        fitness += (output - row[11]) ** 2
+    return 1.0 / fitness
 
-
-def need_resources(task, resources):
-    n = 0
-    if task.type == 3:
-        if resources[0] > 0:
-            n += 1
-        if resources[1] > 0:
-            n += 1
-    elif task.type == 2:
-        if resources[2] > 0:
-            n += 1
-        if resources[1] > 0:
-            n += 1
-    elif task.type == 1:
-        if resources[0] > 0:
-            n += 1
-        if resources[2] > 0:
-            n += 1
-    return n
-
-
-def allocate_resources(task, resources):
-    if task.type == 3:
-        resources[0] -= 1
-        resources[1] -= 1
-    elif task.type == 2:
-        resources[2] -= 1
-        resources[1] -= 1
-    elif task.type == 1:
-        resources[0] -= 1
-        resources[2] -= 1
-
-
-def release_resources(task, resources):
-    if task.type == 3:
-        resources[0] += 1
-        resources[1] += 1
-    elif task.type == 2:
-        resources[2] += 1
-        resources[1] += 1
-    elif task.type == 1:
-        resources[2] += 1
-        resources[0] += 1
-
-
-def add_to_waiting(task):
-    waiting_queue.append(task)
-
-
-def remove_from_waiting(task, resources):
-    if check_resources(task, resources):
-        waiting_queue.remove(task)
-
-
-def show_waiting_tasks():
-    for task in waiting_queue:
-        print(task)
-
-
-def print_status(resource, running, priority_queue, waiting_queue):
-    print(f"R1: {resource[0]} R2:  {resource[1]}  R3: {resource[2]}")
-    print(f"priority_queue:{[t.name for t in priority_queue]}")
-    # if(running!=None):
-    print(f"running:{running.name}")
-    print(f"waiting_queue:{[t.name for t in waiting_queue]}")
-
-
-
-def calculate_response_ratio(task):
-    if task.waiting_time != 0:
-        response_ratio = (task.waiting_time + task.execution_time) / task.execution_time
-        return response_ratio
-    else:
-        return task.type
-
-
-def HRRN(tasks, resources):
-    current_time = 0
-    running = None
-    while len(tasks) > 0 or running is not None:
-        # response_ratios = [calculate_response_ratio(task, current_time) for task in waiting_queue]
-        #     max_index = max(range(len(response_ratios)), key=response_ratios.__getitem__)
-        #     selected_task = waiting_queue[max_index]
-        tasks.sort(key=calculate_response_ratio)
-        while running is None:
-            task = tasks.pop(0)
-            if check_resources(task, resources):
-                allocate_resources(task, resources)
-                running = task
-            else:
-                add_to_waiting(task)
-        running.execution_time -= 1
-        print_status(resources, running, tasks, waiting_queue)
-        for t in tasks:
-            if t != running:
-                t.waiting_time += 1
-        if running.execution_time == 0:
-            release_resources(running, resources)
-            running = None
-
-        for i in range(len(waiting_queue)):
-            task = waiting_queue.pop(0)
-            if check_resources(task, resources):
-                tasks.append(task)
-            else:
-                add_to_waiting(task)
-    # while tasks or waiting_queue:
-    #     # Check if there are any new tasks arriving at the current time
-    #     for task in tasks:
-    #         if task.execution_time <= current_time:
-    #             add_to_waiting(task)
-    #             tasks.remove(task)
-    #     if not waiting_queue:
-    #         current_time += 1
-    #         continue
-    #     response_ratios = [calculate_response_ratio(task, current_time) for task in waiting_queue]
-    #     max_index = max(range(len(response_ratios)), key=response_ratios.__getitem__)
-    #     selected_task = waiting_queue[max_index]
-    #
-    #     if check_resources(selected_task, resources):
-    #         allocate_resources(selected_task, resources)
-    #         selected_task.waiting_time = current_time
-    #         time.sleep(selected_task.execution_time)
-    #         release_resources(selected_task, resources)
-    #         waiting_queue.remove(selected_task)
-    #     else:
-    #         current_time += 1
-    #
-    # print("All tasks completed.")
-
-
-def SJF(tasks, resources):
-    running = None
-    tasks.sort(key=lambda t: t.execution_time)
-    while len(tasks) > 0 or running is not None:
-        while running == None:
-            task = tasks.pop(0)
-            # task = heapq.heappop([(t.execution_time,t) for t in t])
-            if check_resources(task, resources):
-                allocate_resources(task, resources)
-                running = task
-            else:
-                add_to_waiting(task)
-
-        running.execution_time -= 1
-        print_status(resources, running, tasks, waiting_queue)
-        if running.execution_time == 0:
-            release_resources(running, resources)
-            running = None
-        changed = False
-        for i in range(len(waiting_queue)):
-            task = waiting_queue.pop(0)
-            # task = heapq.heappop(waiting_queue, key=lambda t: t.execution_time)
-            if check_resources(task, resources):
-                tasks.append(task)
-                changed = True
-            else:
-                add_to_waiting(task)
-        if changed:
-            tasks.sort(key=lambda t: t.execution_time)
 
 if __name__ == "__main__":
-    R1 = int(input("number of resources for R1: "))
-    resources.append(R1)
-    R2 = int(input("number of resources for R2: "))
-    resources.append(R2)
-    R3 = int(input("number of resources for R3: "))
-    resources.append(R3)
-    print(resources)
-    num_tasks = int(input("number of tasks: "))
-    for i in range(num_tasks):
-        task_name = input("name of task: ")
-        task_type = input("type of task: ")
-        task_duration = int(input("time of task: "))
-        tasks.append(Task(task_name, task_type, task_duration))
-    choose = input("choose the algorithm: ")
-    if choose == 'SJF':
-        SJF(tasks, resources)
-    # elif choose == 'FCFS':
-    #     # FCFS(tasks, resources)
-    # if choose == 'RR':
-    #     time_slice = int(input("time slice: "))
-    #     # Round_Robin(tasks, resources, time_slice)
-    if choose == 'HRRN':
-        # arrival_time = int(input("Arrival time: "))
-        HRRN(tasks, resources)
+    train_data, test_data = get_data()
 
-    # print("list of tasks:")
-    # for task in tasks:
-    #     print(task)
+    fitness_function = fitness_func
+
+    ga_instance = pygad.GA(num_generations=50,
+                           num_parents_mating=4,
+                           fitness_func=fitness_function,
+                           sol_per_pop=8,
+                           num_genes=11,
+                           parent_selection_type="sss",
+                           keep_parents=1,
+                           crossover_type="single_point",
+                           mutation_type="random",
+                           mutation_percent_genes=10)
+
+    ga_instance.run()
+
+    solution, solution_fitness, solution_idx = ga_instance.best_solution()
+    print("Parameters of the best solution : {solution}".format(solution=solution))
+    print("Fitness value of the best solution = {solution_fitness}".format(solution_fitness=solution_fitness))
+    print("Parameters of the best solution : {solution_idx}".format(solution_idx=solution_idx))
+
+    print(test(ga_instance, solution, solution_idx))
+
